5216b71c3c27dc6ca41b574de3c8977a823e1fa2
sean <custodiocsean@gmail.com>
2022-11-10 14:24:07 +0800

    register and login controller

--- server/src/config/ConnectDB.ts
+++ server/src/config/ConnectDB.ts
@@ -2,7 +2,6 @@ import mysql from "mysql";
 
 export const db = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "M3ongchan23#",
   database: "chat",
--- server/src/controller/AuthController.ts
+++ server/src/controller/AuthController.ts
@@ -1,25 +1,50 @@
 import { Request, Response } from "express";
 import { db } from "../config/ConnectDB";
-
-export const login_controller = (req: Request, res: Response) => {
-  res.send("login page");
-};
+import bcrypt from "bcrypt";
 
 export const register_controller = (req: Request, res: Response) => {
   const first_name = req.body.first_name;
   const last_name = req.body.last_name;
   const email = req.body.email;
   const password = req.body.password;
+  const date_now = new Date();
   // check if user exist
-  const q = "SELECT FROM user WHERE email = ?";
+  const sql_check = "SELECT * FROM user WHERE email = ?";
 
-  db.query(q, [email], (err, data) => {
-    if (err) return res.status(500).json({ error: "user dont exist" });
+  db.query(sql_check, [email], async (err, data) => {
+    if (err) return res.status(409).json(err);
     if (data.length)
       return res.status(409).json({ error: "user already exist" });
-  });
-  // create new user
-  // hash password with bcrypt
 
-  // store it in the database
+    const salt = await bcrypt.genSalt(10);
+    const hash_password = await bcrypt.hash(password, salt);
+
+    const sql_insert =
+      "INSERT INTO user (first_name, last_name, email, date_created, password) VALUES (?)";
+
+    const values = [first_name, last_name, email, date_now, hash_password];
+
+    db.query(sql_insert, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(values);
+    });
+  });
+};
+
+export const login_controller = async (req: Request, res: Response) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const sql_check = "SELECT * FROM user WHERE email = ?";
+
+  db.query(sql_check, [email], async (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (!data.length) res.status(404).json({ msg: "user dont exist" });
+
+    const check_password = await bcrypt.compare(password, data[0].password);
+
+    if (!check_password) return res.status(404).json({ err: "wrong password" });
+
+    return res.status(200).json(data);
+  });
 };
